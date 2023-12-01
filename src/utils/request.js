dd38afecd07a6136409102b9298e04d197b42fa2
Haoqinghai <1850427828@qq.com>
2023-12-01 13:21:27 +0800

    合并冲突

--- src/utils/request.js
+++ src/utils/request.js
@@ -70,17 +70,17 @@ service.interceptors.response.use(res => {
     return res.data
   }
   if (code === 401) {
-    if (!isRelogin.show) {
-      isRelogin.show = true;
-      ElMessageBox.confirm('登录状态已过期，您可以继续留在该页面，或者重新登录', '系统提示', { confirmButtonText: '重新登录', cancelButtonText: '取消', type: 'warning' }).then(() => {
-        isRelogin.show = false;
-        useUserStore().logOut().then(() => {
-          location.href = import.meta.env.VITE_APP_CONTEXT_PATH + 'index';
-        })
-      }).catch(() => {
-        isRelogin.show = false;
-      });
-    }
+    // if (!isRelogin.show) {
+    //   isRelogin.show = true;
+    //   ElMessageBox.confirm('登录状态已过期，您可以继续留在该页面，或者重新登录', '系统提示', { confirmButtonText: '重新登录', cancelButtonText: '取消', type: 'warning' }).then(() => {
+    //     isRelogin.show = false;
+    //     useUserStore().logOut().then(() => {
+    //       location.href = import.meta.env.VITE_APP_CONTEXT_PATH + 'index';
+    //     })
+    //   }).catch(() => {
+    //     isRelogin.show = false;
+    //   });
+    // }
     return Promise.reject('无效的会话，或者会话已过期，请重新登录。')
   } else if (code === 500) {
     ElMessage({ message: msg, type: 'error' })
@@ -91,11 +91,7 @@ service.interceptors.response.use(res => {
   } else if (code !== 200) {
     ElNotification.error({ title: msg })
     return Promise.reject('error')
-  } else if (code !== 30000) {
-    ElMessage({ message: msg, type: 'warning' })
-    
-  }
-   else {
+  } else {
     return Promise.resolve(res.data)
   }
 }, error => {
